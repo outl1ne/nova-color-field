b6a9aadac6d55bd5b781e29176337f5262309883
Tarvo Reinpalu <tarvoreinpalu@gmail.com>
2022-05-24 16:03:29 +0300

    Update dist files

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,13 +1,13 @@
-let mix = require('laravel-mix')
-let tailwindcss = require('tailwindcss')
-let path = require('path')
-let postcssImport = require('postcss-import')
+let mix = require('laravel-mix');
+let tailwindcss = require('tailwindcss');
+let path = require('path');
+let postcssImport = require('postcss-import');
 
-require('./nova.mix.js')
+require('./nova.mix.js');
 
 mix
   .setPublicPath('dist')
   .js('resources/js/field.js', 'js')
   .vue({ version: 3 })
-  .postCss('resources/css/field.css', 'dist/css/', [postcssImport(), tailwindcss('tailwind.config.js'),])
-  .nova('optimistdigital/nova-color-fields')
+  .postCss('resources/css/field.css', 'dist/css/', [postcssImport(), tailwindcss('tailwind.config.js')])
+  .nova('outl1ne/nova-color-fields');
