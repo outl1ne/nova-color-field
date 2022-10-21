41efe20cf0327aad70638e100d878878c97850be
Tarvo R <tarvoreinpalu@gmail.com>
2022-10-21 16:43:23 +0300

    Fix DetailField

--- resources/js/components/DetailField.vue
+++ resources/js/components/DetailField.vue
@@ -2,7 +2,11 @@
   <PanelItem :index="index" :field="field">
     <template #value>
       <div class="o1-inline-flex o1-items-center">
-        <div class="o1-bg-checkered o1-mr-2 o1-rounded o1-overflow-hidden" style="width: 22px; height: 22px">
+        <div
+          class="o1-bg-checkered o1-mr-2 o1-rounded o1-overflow-hidden"
+          style="width: 22px; height: 22px"
+          v-if="field.value"
+        >
           <div class="o1-mr-2 o1-w-full o1-h-full" :style="{ backgroundColor: field.value }" />
         </div>
         <div>{{ field.value || '—' }}</div>
